9d56ca7097976184c30863ec0a80e42e2e4bb982
Alexey Horoshavin <Akela.Klimov@yandex.ru>
2022-09-18 08:43:09 +0700

    add sub lab 3

---
+++ lab_1_3/src/lab_1_3/core.clj
@@ -0,0 +1,14 @@
+(ns lab-1-3.core
+  (:gen-class))
+
+(defn my-map [f coll]
+  (reduce (fn [acc elem] (conj acc (f elem))) [] coll))
+
+(defn my-filter [pred coll]
+  (reduce (fn [acc elem] (if (pred elem) (conj acc elem) acc)) [] coll))
+
+(defn -main [& args]
+  (println(my-map inc [1 2 3 4 5]))
+  (println(my-filter even? (range 10)))
+  (println "My-map and My-filter")
+  )
